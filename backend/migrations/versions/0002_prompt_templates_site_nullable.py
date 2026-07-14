7d7570d709d9d4d045d5565df4c9a5ebf2d0a28f
Anton Kasimov <anton.kasimov@local>
2026-07-14 23:39:43 +0300

    Shorten prompt migration revision

--- backend/migrations/versions/0002_prompt_templates_site_nullable.py
+++ backend/migrations/versions/0002_prompt_templates_site_nullable.py
@@ -1,6 +1,6 @@
 """Allow global prompt templates without a site.
 
-Revision ID: 0002_prompt_templates_site_nullable
+Revision ID: 0002_prompt_site_nullable
 Revises: 0001_current_schema
 Create Date: 2026-07-14
 """
@@ -8,7 +8,7 @@ Create Date: 2026-07-14
 from alembic import op
 
 
-revision = "0002_prompt_templates_site_nullable"
+revision = "0002_prompt_site_nullable"
 down_revision = "0001_current_schema"
 branch_labels = None
 depends_on = None
